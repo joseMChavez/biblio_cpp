47f8f6d4d9375b3d28bbc572dbcd7eb34afebb0a
José Chávez <josemchavez0123@hotmail.com>
2026-05-04 00:14:00 +0200

    Add update method to IBookService interface

--- book_api/services/IBookService.h
+++ book_api/services/IBookService.h
@@ -11,8 +11,7 @@ public:
     virtual Result<Book> getById(int id) = 0;
     virtual Result<Book> create(const Book& libro) = 0;
     virtual Result<bool> remove(int id) = 0;
-<<<<<<< HEAD
+ 
     virtual Result<bool> update(const Reader& item) = 0;
-=======
->>>>>>> de6f692cc521b603432362c4212f7a0d55aa9687
+ 
 };
